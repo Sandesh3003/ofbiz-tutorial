e67ad67a112c0fef282c701743e7330d584b4116
Sandesh Ratnawat <sandeshratnawat20320@acropolis.in>
2024-04-05 16:19:41 +0530

    Improved: update in service definition to accept partyId in input parameters and update in service implementation createUserByJavaService
    Updated service definition of the createPostalAddressJava, createTelecomNumberJava and createEmailAddressJava to accept partyId as input parameter.

--- src/main/java/org/apache/ofbiz/ofbiztutorial/usermgr/services/UserMgrServices.java
+++ src/main/java/org/apache/ofbiz/ofbiztutorial/usermgr/services/UserMgrServices.java
@@ -4,10 +4,7 @@ package org.apache.ofbiz.ofbiztutorial.usermgr.services;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.base.util.UtilMisc;
 import org.apache.ofbiz.entity.GenericValue;
-import org.apache.ofbiz.service.DispatchContext;
-import org.apache.ofbiz.service.GenericServiceException;
-import org.apache.ofbiz.service.ModelService;
-import org.apache.ofbiz.service.ServiceUtil;
+import org.apache.ofbiz.service.*;
 import org.apache.ofbiz.base.util.UtilDateTime;
 
 
@@ -19,48 +16,59 @@ public class UserMgrServices{
 
     public static Map<String, Object> createUserByJavaService(DispatchContext dctx, Map<String, ? extends Object> context) throws GenericServiceException {
         Map<String, Object> result = ServiceUtil.returnSuccess();
-        Map<String, Object> serviceContext = null;
+        LocalDispatcher dispatcher= dctx.getDispatcher();
+        Map<String, Object> serviceContext = new HashMap<>();
+
         try{
             serviceContext=dctx.makeValidContext("createPersonAndUserLogin", ModelService.IN_PARAM, context);
-            Map<String, Object> createPersonResult = dctx.getDispatcher().runSync("createPersonAndUserLogin", serviceContext);
-            if (ServiceUtil.isError(createPersonResult)) {
-                return createPersonResult;
+            result = dispatcher.runSync("createPersonAndUserLogin", serviceContext);
+            if (ServiceUtil.isError(result)) {
+                return result;
             }
-            GenericValue newUserLogin = (GenericValue) createPersonResult.get("newUserLogin");
+            String partyId= result.get("partyId").toString();
+            GenericValue newUserLogin = (GenericValue) result.get("newUserLogin");
             serviceContext.clear();
+            result.clear();
 
             serviceContext = dctx.makeValidContext("addUserLoginToSecurityGroup", ModelService.IN_PARAM, context);
             serviceContext.put("groupId", "PARTYADMIN");
-            Map<String, Object> addSecurityGroupResult = dctx.getDispatcher().runSync("addUserLoginToSecurityGroup", serviceContext);
-            if(ServiceUtil.isError(addSecurityGroupResult)){
-                return addSecurityGroupResult;
+            serviceContext.put("fromDate", UtilDateTime.nowDate());
+            result = dispatcher.runSync("addUserLoginToSecurityGroup", serviceContext);
+            if(ServiceUtil.isError(result)){
+                return result;
             }
             serviceContext.clear();
+            result.clear();
 
             serviceContext=dctx.makeValidContext("createPostalAddressJava", ModelService.IN_PARAM, context);
-            serviceContext.replace("userLogin", newUserLogin);
-            Map<String, Object> createPostalAddressResult = dctx.getDispatcher().runSync("createPostalAddressJava", serviceContext);
-            if (ServiceUtil.isError(createPostalAddressResult)) {
-                return createPostalAddressResult;
+            serviceContext.put("partyId",  partyId);
+            result = dispatcher.runSync("createPostalAddressJava", serviceContext);
+            if (ServiceUtil.isError(result)) {
+                return result;
             }
             serviceContext.clear();
+            result.clear();
 
             serviceContext = dctx.makeValidContext("createTelecomNumberJava", ModelService.IN_PARAM, context);
-            serviceContext.replace("userLogin", newUserLogin);
-            Map<String, Object> createTelecomNumberResult= dctx.getDispatcher().runSync("createTelecomNumberJava", serviceContext);
-            if (ServiceUtil.isError(createTelecomNumberResult)) {
-                return createTelecomNumberResult;
+            serviceContext.put("partyId",  partyId);
+            result= dispatcher.runSync("createTelecomNumberJava", serviceContext);
+            if (ServiceUtil.isError(result)) {
+                return result;
             }
             serviceContext.clear();
+            result.clear();
 
             serviceContext = dctx.makeValidContext("createEmailAddressJava", ModelService.IN_PARAM, context);
-            serviceContext.replace("userLogin", newUserLogin);
-            Map<String, Object> createEmailAddressResult = dctx.getDispatcher().runSync("createEmailAddressJava", serviceContext);
-            if (ServiceUtil.isError(createEmailAddressResult)) {
-                return createEmailAddressResult;
+            serviceContext.put("partyId",  partyId);
+            result = dispatcher.runSync("createEmailAddressJava", serviceContext);
+            if (ServiceUtil.isError(result)) {
+                return result;
             }
+            serviceContext.clear();
+            result.clear();
+
             result.put("newUserLogin", newUserLogin);
-            result.put("partyId", createPersonResult.get("partyId").toString());
+            result.put("partyId", partyId);
         }catch (GenericServiceException e) {
             return ServiceUtil.returnError("Error creating User !!!! " + e.getMessage());
         }
